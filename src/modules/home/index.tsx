ea0e1d1b85cd2e56b89265e50efff0185489b4c4
M.Hassan Mojab <mh_mojab75@yahoo.com>
2022-02-01 16:46:07 +0330

    add loading spinner

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -7,11 +7,17 @@ import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 import { useProducts } from './data/HomeQueryHooks';
 import ProductItem from './components/product-item';
 import { usePaginatedList } from './hooks/usePaginatedList';
+import { PulseLoader } from 'react-spinners';
 
 const Home = () => {
   const { ref, page, setPage, pageSize } = usePaginatedList();
 
-  const { data: products, isLoading, error, isFetching } = useProducts({ page, size: pageSize });
+  const {
+    data: products,
+    isLoading,
+    error,
+    isPreviousData,
+  } = useProducts({ page, size: pageSize });
 
   const pageCount = useMemo(() => {
     return products ? Math.floor(products.meta.pagination.total / pageSize) + 1 : 10;
@@ -29,8 +35,8 @@ const Home = () => {
   }, [pageSize, products?.product_variations.length]);
 
   const renderList = useCallback(() => {
-    if (isLoading || isFetching) {
-      return <div>loading...</div>;
+    if (isLoading) {
+      return null;
     }
 
     if (error) {
@@ -50,11 +56,25 @@ const Home = () => {
         ))}
       </div>
     );
-  }, [isLoading, isFetching, error, products?.product_variations, fillArray]);
+  }, [isLoading, error, products?.product_variations, fillArray]);
+
+  const renderLoading = useCallback(() => {
+    if (isLoading || isPreviousData) {
+      return (
+        <div className="absolute top-0 left-0 bottom-0 right-0 flex justify-center items-center backdrop-blur-sm">
+          <PulseLoader color="var(--color-accent)" size={26} />
+        </div>
+      );
+    }
+    return null;
+  }, [isLoading, isPreviousData]);
 
   return (
     <div ref={ref} className="flex flex-col flex-1 h-full bg-gray-100">
-      <div className="flex flex-1 overflow-hidden">{renderList()}</div>
+      <div className="flex flex-1 overflow-hidden relative">
+        {renderList()}
+        {renderLoading()}
+      </div>
 
       <div dir="ltr">
         <ReactPaginate
